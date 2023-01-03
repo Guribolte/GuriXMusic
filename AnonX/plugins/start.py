90be4341e56c08bb3f4d3634e4b765d1a616e8c9
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-01-03 18:34:19 +0100

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAJE8GK4EsoLVZC2SW5W5Q-QAkaoN8f_AAL9BQACiy14VGoQxOCDfE1KKQQ")
+            await message.reply_sticker("CAACAgQAAxkBAAEBcPVjtGbQjtQkNmcBBwTQBgEGqZZivAACKQ8AAjDLUVLQkBuzJmXU1y0E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"], reply_markup=keyboard
