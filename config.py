01ce251d7d0b01017ca01db02609bfc33544f400
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-01-09 21:02:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ STREAM_IMG_URL = "https://telegra.ph//file/a9e6b033f11188715c316.jpg"
 
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph//file/e2a332c4222fb05aa14ae.jpg"
 
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 
