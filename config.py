160621609bb5055f02afd0f3dd7a388f0cb9933d
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-01-13 00:56:21 +0100

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5034422341").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Guribolte/GuriXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", None)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Guri")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
