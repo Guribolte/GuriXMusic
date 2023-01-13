0a99d75a495cda65c98a0e8ff4e5f9fd18fec2c5
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-01-13 22:45:02 +0100

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -8,39 +8,7 @@ from AnonX.utils.formatters import time_to_seconds
 
 ## After Edits with Timer Bar
 
-def stream_markup_timer(_, videoid, chat_id, played, dur):
-    played_sec = time_to_seconds(played)
-    duration_sec = time_to_seconds(dur)
-    percentage = (played_sec / duration_sec) * 100
-    anon = math.floor(percentage)
-    if 0 < anon <= 10:
-        bar = "⬤─────────"
-    elif 10 < anon < 20:
-        bar = "━⬤────────"
-    elif 20 <= anon < 30:
-        bar = "━━⬤───────"
-    elif 30 <= anon < 40:
-        bar = "━━━⬤──────"
-    elif 40 <= anon < 50:
-        bar = "━━━━⬤─────"
-    elif 50 <= anon < 60:
-        bar = "━━━━━⬤────"
-    elif 60 <= anon < 70:
-        bar = "━━━━━━⬤───"
-    elif 70 <= anon < 80:
-        bar = "━━━━━━━⬤──"
-    elif 80 <= anon < 95:
-        bar = "━━━━━━━━⬤─"
-    else:
-        bar = "━━━━━━━━━⬤"
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
-        [
+
             InlineKeyboardButton(
                 text="▷",
                 callback_data=f"ADMIN Resume|{chat_id}",
@@ -67,34 +35,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     return buttons
 
 
-def telegram_markup_timer(_, chat_id, played, dur):
-    played_sec = time_to_seconds(played)
-    duration_sec = time_to_seconds(dur)
-    percentage = (played_sec / duration_sec) * 100
-    anon = math.floor(percentage)
-    if 0 < anon <= 10:
-        bar = "⬤─────────"
-    elif 10 < anon < 20:
-        bar = "━⬤────────"
-    elif 20 <= anon < 30:
-        bar = "━━⬤───────"
-    elif 30 <= anon < 40:
-        bar = "━━━⬤──────"
-    elif 40 <= anon < 50:
-        bar = "━━━━⬤─────"
-    elif 50 <= anon < 60:
-        bar = "━━━━━⬤────"
-    elif 60 <= anon < 70:
-        bar = "━━━━━━⬤───"
-    elif 70 <= anon < 80:
-        bar = "━━━━━━━⬤──"
-    elif 80 <= anon < 95:
-        bar = "━━━━━━━━⬤─"
-    else:
-        bar = "━━━━━━━━━⬤"
 
-    buttons = [
-        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
