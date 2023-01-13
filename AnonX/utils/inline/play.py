83b82009dfabe7970f39833dc1194bdf9d197d60
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-01-13 22:53:02 +0100

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -14,26 +14,25 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     anon = math.floor(percentage)
     if 0 < anon <= 10:
-        bar = "◉—————————"
+        bar = "⬤─────────"
     elif 10 < anon < 20:
-        bar = "—◉————————"
+        bar = "━⬤────────"
     elif 20 <= anon < 30:
-        bar = "——◉———————"
+        bar = "━━⬤───────"
     elif 30 <= anon < 40:
-        bar = "———◉——————"
+        bar = "━━━⬤──────"
     elif 40 <= anon < 50:
-        bar = "————◉—————"
+        bar = "━━━━⬤─────"
     elif 50 <= anon < 60:
-        bar = "—————◉————"
+        bar = "━━━━━⬤────"
     elif 60 <= anon < 70:
-        bar = "——————◉———"
+        bar = "━━━━━━⬤───"
     elif 70 <= anon < 80:
-        bar = "———————◉——"
+        bar = "━━━━━━━⬤──"
     elif 80 <= anon < 95:
-        bar = "————————◉—"
+        bar = "━━━━━━━━⬤─"
     else:
-        bar = "—————————◉"
-
+        bar = "━━━━━━━━━⬤"
     buttons = [
         [
             InlineKeyboardButton(
@@ -74,26 +73,25 @@ def telegram_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     anon = math.floor(percentage)
     if 0 < anon <= 10:
-        bar = "◉—————————"
+              bar = "⬤─────────"
     elif 10 < anon < 20:
-        bar = "—◉————————"
+        bar = "━⬤────────"
     elif 20 <= anon < 30:
-        bar = "——◉———————"
+        bar = "━━⬤───────"
     elif 30 <= anon < 40:
-        bar = "———◉——————"
+        bar = "━━━⬤──────"
     elif 40 <= anon < 50:
-        bar = "————◉—————"
+        bar = "━━━━⬤─────"
     elif 50 <= anon < 60:
-        bar = "—————◉————"
+        bar = "━━━━━⬤────"
     elif 60 <= anon < 70:
-        bar = "——————◉———"
+        bar = "━━━━━━⬤───"
     elif 70 <= anon < 80:
-        bar = "———————◉——"
+        bar = "━━━━━━━⬤──"
     elif 80 <= anon < 95:
-        bar = "————————◉—"
+        bar = "━━━━━━━━⬤─"
     else:
-        bar = "—————————◉"
-
+        bar = "━━━━━━━━━⬤"
     buttons = [
         [
             InlineKeyboardButton(
