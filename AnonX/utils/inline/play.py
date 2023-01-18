65cbf937d538a06557977ba4c8a73cfa7688a211
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-01-18 01:28:14 +0100

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -60,7 +60,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             InlineKeyboardButton(
                 text="+ ᴘʟᴀʏʟɪsᴛ +", callback_data=f"add_playlist {videoid}"
             ),
-            InlineKeyboardButton('SUPPORT', "https://t.me/sharan113baaplogzz")
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"{config.SUPPORT_GROUP}",
+            ),
         ],
         [
             InlineKeyboardButton(
@@ -148,7 +151,10 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(
                 text="+ ᴘʟᴀʏʟɪsᴛ +", callback_data=f"add_playlist {videoid}"
             ),
-            InlineKeyboardButton('SUPPORT', "https://t.me/sharan113baaplogzz")
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"{config.SUPPORT_GROUP}",
+            ),
          ],
          [
             InlineKeyboardButton(
