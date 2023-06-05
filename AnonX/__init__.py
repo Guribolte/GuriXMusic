d750ffc16ec3777d44983a75d1f39b2a8a0b194b
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-06-05 14:37:15 +0200

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -35,3 +35,46 @@ SoundCloud = SoundAPI()
 Telegram = TeleAPI()
 
 aiohttpsession = ClientSession()
+from AnonX.core.bot import AnonXBot
+
+from AnonX.core.dir import dirr
+
+from AnonX.core.git import git
+
+from AnonX.core.userbot import Userbot
+
+from AnonX.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+
+app = AnonXBot()
+
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+
+Carbon = CarbonAPI()
+
+Spotify = SpotifyAPI()
+
+Apple = AppleAPI()
+
+Resso = RessoAPI()
+
+SoundCloud = SoundAPI()
+
+Telegram = TeleAPI()
