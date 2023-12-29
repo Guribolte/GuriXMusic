6bb90704b0e208f53364bce1572c23d39342ab82
Guribolte <101675322+Guribolte@users.noreply.github.com>
2023-12-29 18:59:16 +0000

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic import app
+from GuriXMusic import app
 from config import YOUTUBE_IMG_URL
 
 
